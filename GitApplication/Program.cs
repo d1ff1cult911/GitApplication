19cf48afc385c697f461d6eadb6a7eae3128795b
d1ff1cult911 <alivklimov@gmail.com>
2023-02-01 21:30:32 +0300

    Добавлена функция MatrixA

--- GitApplication/Program.cs
+++ GitApplication/Program.cs
@@ -6,7 +6,29 @@ namespace GitApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+        public static void MatrixA(int a, int b)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Random rnd = new Random();
+            
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    int n = rnd.Next(1, 100);
+                    if (n %2==0)
+                    {
+                        Console.Write("0 ");
+                    }
+                    else
+                    {
+                        Console.Write("1 ");
+                    }
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
